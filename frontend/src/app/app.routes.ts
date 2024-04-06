909d5763acb45985d3d9ed3b15c3fd6691dcea50
Magda98 <magdakochman7@gmail.com>
2024-04-06 19:48:58 +0200

    add about, landing-page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,20 +1,45 @@
 import { Routes } from '@angular/router';
 import { MapComponent } from './map/map.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { DangerousPlacesComponent } from './dangerous-places/dangerous-places.component';
 
 export const routes: Routes = [
   {
-    path: 'map',
-    component: MapComponent,
+    path: '',
     pathMatch: 'full',
-    title: 'Mapa',
-    data: {
-      title: 'Mapa',
-    },
+    title: 'WheelJourney',
+    component: HomeComponent,
   },
   {
     path: '',
-    pathMatch: 'full',
-    component: AboutComponent,
+    component: NavComponent,
+    children: [
+      {
+        component: MapComponent,
+        path: 'map',
+        title: 'WheelJourney - mapa',
+        data: {
+          title: 'Mapa',
+        },
+      },
+      {
+        component: AboutComponent,
+        path: 'about',
+        title: 'WheelJourney - about',
+        data: {
+          title: 'Informacje',
+        },
+      },
+      {
+        component: DangerousPlacesComponent,
+        path: 'dangerous-places',
+        title: 'WheelJourney - niebezpieczne miejsca',
+        data: {
+          title: 'Niebezpiecznie miejsca',
+        },
+      },
+    ],
   },
 ];
