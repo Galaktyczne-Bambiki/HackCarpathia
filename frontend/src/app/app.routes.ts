b47a1f3172fdb7c3f42c01b109858a3f08f03f00
Magda98 <magdakochman7@gmail.com>
2024-04-06 16:45:35 +0200

    add app layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { MapComponent } from './map/map.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'map',
+    component: MapComponent,
+    pathMatch: 'full',
+    title: 'Mapa',
+    data: {
+      title: 'Mapa',
+    },
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: AboutComponent,
+  },
+];
